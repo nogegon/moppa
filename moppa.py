7dcbb243ad2f27101cf7bc4b2756c3ac23f84c66
Olivier Gaulard <43503071+nogegon@users.noreply.github.com>
2019-07-25 13:34:26 +0200

    Removed project description from comments

--- moppa.py
+++ moppa.py
@@ -1,34 +1,6 @@
 # My Own Python Plugin Attempt (MOPPA)
 #
-# Yes, I like to give (stupid) names
-#
 # By Olivier Gaulard
-# 
-# The objective/concept is to have :
-#   - Several "plugins" (Python Classes in fact) that are architectured in the same way (like a template)
-#   - The code does not know in advance the names of the files (called plugins)
-#   - It is just expecting some functions to be present, callable and returning values (if needed)
-#   - One plugin that is used at a time (active_plugin). But it can be changed easily
-#
-# The structure is as follows : 
-# moppa.py [This file]
-# plugins/pluginA.py
-# plugins/pluginB.py
-# plugins/etc...
-# 
-# MOPPA will cycle through all the files in the plugins directory.
-# You define your own way of selecting/identifying the correct plugin you want to work with (function check_me() in this exemple).
-#
-# You want to add a new plugin ? 
-#   Just use the same template you have used for the others 
-#   Put it into the plugin directory
-#   Rescan
-
-# In this demo, you will see some exemples of using/accessing variables, functions
-# and passing some values between this file and the loaded plugin (and the other way around).
-
-# Finally, I'm not a developper. MOPPA is just a way to have fun with Python and to (eventually) use it in another project
-
 # 
 #
 # 
